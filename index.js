435b607a315aa6b9d48f73e0aa8f78fd3a356d0e
rk-crud <94461045+rk-crud@users.noreply.github.com>
2021-12-02 13:25:19 +0530

    Add files via upload
    Assignment 2 files uploaded

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require('express');
+const express = require('express')
+// const cookieParser = require('cookie-parser')
 const greetingsRoute = require('./greetings')
 const travelRoute = require('./travel')
-const PORT = process.env.SERVER_PORT || 3000;
-const server = express();
+const blogRoute = require('./user')
+const PORT = process.env.SERVER_PORT || 4000
+const server = express()
+
+server.use(express.json());
+// server.use(cookieParser());
+
+// function loggerMiddleware(req,res,next){
+//   console.log(`LOG: ${req.originalUrl} called on at ${Date().toString()}`)
+//   console.log(`${req.originalUrl} called by ${req.param['name']}`)
+//   next()
+// }
+// server.use(loggerMiddleware)
 
 server.get('/', function(req, res) {
   res.send('Welcome to my API!')
@@ -10,6 +22,9 @@ server.get('/', function(req, res) {
 
 server.use('/greetings', greetingsRoute)
 server.use('/travel', travelRoute)
+server.use('/user',blogRoute) //created new route for post
+
+
 
 server.listen(PORT, function () {
   console.log(`Server started and listening on http://localhost:${PORT}/`);
